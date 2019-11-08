400f69a71de6344172a6a24690542915c3c27fa1
Pete James <petejames@petes-air.lan>
2019-11-07 19:55:51 -0800

    mvp complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,8 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-const axiosPromise = axios.get("https://api.github.com/users/petedram");
-console.log(axiosPromise);
+let axiosPromise = axios.get("https://api.github.com/users/petedram");
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -27,7 +26,7 @@ console.log(axiosPromise);
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -63,18 +62,6 @@ function createCard(data) {
   const following = document.createElement('p');
   const bio = document.createElement('p');
 
-    // Setup structure of elements
-  card.appendChild(cardImg)
-  card.appendChild(cardInfo)
-  cardInfo.appendChild(name)
-  cardInfo.appendChild(username)
-  cardInfo.appendChild(location)
-  cardInfo.appendChild(profile)
-  profile.appendChild(profileUrl)
-  cardInfo.appendChild(followers)
-  cardInfo.appendChild(following)
-  cardInfo.appendChild(bio)
-
     // set class names
   card.classList.add('card')
   cardInfo.classList.add('card-info')
@@ -84,23 +71,44 @@ function createCard(data) {
   // set text content
   cardImg.src = data.avatar_url
   name.textContent = data.name
-  //continue from here tomorrow
+  username.textContent = data.login
+  location.textContent = data.location
+  profile.textContent = 'Profile: '
+  profileUrl.textContent = data.html_url
+  profileUrl.href = data.html_url
+  followers.textContent = data.followers
+  following.textContent = data.following
+  bio.textContent = data.bio
+
+  // Setup structure of elements
+  card.appendChild(cardImg)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(profileUrl)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)   
+
+return card;
+};
+
+const cardObject = document.querySelector('.cards');
 
 
+axiosPromise.then(response => {
+  cardObject.appendChild(createCard(response.data));
+  console.log(response.data);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+followersArray.forEach(item => {
+  let axiosPromise = axios.get("https://api.github.com/users/" + item);
+  axiosPromise.then(response => {
+    cardObject.appendChild(createCard(response.data));
+  });
+})
 
 
 /* List of LS Instructors Github username's: 
